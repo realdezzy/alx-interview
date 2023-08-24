cccee7ff016a3c43fc0144645e0a64beec51f618
realdezzy <realdezzy1@gmail.com>
2023-08-25 00:06:47 +0100

    DP sol

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Make Change"""
+
+
+def makeChange(coins, total):
+    """ Make change solution using dynamic programming """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
