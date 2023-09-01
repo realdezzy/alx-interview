45deaecc4e67949e3622b1ce7e6b5da0263d37d3
realdezzy <realdezzy1@gmail.com>
2023-09-01 01:08:19 +0100

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """Island perimeter implementation"""
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
